314f06b878d4f913ccaf611fb8ea0c0f8d81a25d
수정계곡 <hamkegaja2@gmail.com>
2024-03-19 22:03:33 +0900

    파일정리

--- LizardCorpPlatform.Common/GlobalSuppressions.cs
+++
@@ -1,9 +0,0 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given
-// a specific target and scoped to a namespace, type, member, etc.
-
-using System.Diagnostics.CodeAnalysis;
-
-[assembly: SuppressMessage("StyleCop.CSharp.LayoutRules", "SA1503:Braces should not be omitted", Justification = "<보류 중>")]
-[assembly: SuppressMessage("StyleCop.CSharp.OrderingRules", "SA1206:Declaration keywords should follow order", Justification = "<보류 중>")]
